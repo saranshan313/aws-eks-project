167243445c26d2ddf3ad47fbf2369acbba5717ca
saranshan313 <sar.mca05@gmail.com>
2024-04-21 20:06:43 +1000

    add gtw endpoint

--- terraform/06-vpcendpoints.tf
+++ terraform/06-vpcendpoints.tf
@@ -1,15 +1,19 @@
-
-resource "aws_security_group" "vpc_endpoints_sg" {
-  for_each = { for tuple in local.settings.eks_cluster.vpc_endpoints : tuple.id => tuple }
+# Security group for interface VPC endpoints
+resource "aws_security_group" "interface_vpce_sg" {
+  for_each = { for tuple in local.settings.eks_cluster.interface_vpce : tuple.id => tuple }
   name = format(
     "secgrp-%s-%s-vpce-%s-01",
     local.settings.env,
     local.settings.region,
-    upper(each.key)
+    replace(
+      each.key,
+      strcontains(each.key, ".") ? "." : null,
+      "-"
+    )
   )
   description = format(
     "VPC endpoint security group for %s",
-    upper(each.key)
+    each.key
   )
   vpc_id = data.terraform_remote_state.vpc.outputs.network_vpc_id
 
@@ -18,7 +22,7 @@ resource "aws_security_group" "vpc_endpoints_sg" {
     content {
       description = format(
         "Allow access to subnets for %s",
-        upper(each.key)
+        each.key
       )
       from_port = ingress.value
       to_port   = ingress.value
@@ -28,10 +32,23 @@ resource "aws_security_group" "vpc_endpoints_sg" {
       ]
     }
   }
+  tags = {
+    Name = format(
+      "secgrp-%s-%s-vpce-%s-01",
+      local.settings.env,
+      local.settings.region,
+      replace(
+        each.key,
+        strcontains(each.key, ".") ? "." : null,
+        "-"
+      )
+    )
+  }
 }
 
+# Interface VPC endpoints
 resource "aws_vpc_endpoint" "eks_cluster_vpce" {
-  for_each = { for tuple in local.settings.eks_cluster.vpc_endpoints : tuple.id => tuple }
+  for_each = { for tuple in local.settings.eks_cluster.interface_vpce : tuple.id => tuple }
   vpc_id   = data.terraform_remote_state.vpc.outputs.network_vpc_id
   service_name = format(
     "com.amazonaws.%s.%s",
@@ -44,8 +61,89 @@ resource "aws_vpc_endpoint" "eks_cluster_vpce" {
   ]
 
   security_group_ids = [
-    aws_security_group.vpc_endpoints_sg[each.key].id,
+    aws_security_group.interface_vpce_sg[each.key].id,
   ]
 
   private_dns_enabled = true
+
+  tags = {
+    Name = format(
+      "vpce-%s-%s-%s-01",
+      local.settings.env,
+      local.settings.region,
+      replace(
+        each.key,
+        strcontains(each.key, ".") ? "." : null,
+        "-"
+      )
+    )
+  }
+}
+
+#Gateway VPC Endpoints
+locals {
+  # iterate over vpc endpoint gateways nested to the list of route table id
+  route_table_ids = flatten([
+    for tuple in local.settings.vpcendpoints_gateways : [
+      for rt in data.tfe_outputs.infrastructure_infra.values.private_route_table_ids : {
+        vpc_endpoint_id = tuple.id
+        route_table_id  = rt
+      }
+    ]
+  ])
+}
+
+data "aws_vpc_endpoint_service" "vpce_service_gtw" {
+  for_each     = { for tuple in local.settings.vpcendpoints_gateways : tuple.id => tuple }
+  service      = each.key
+  service_type = "Gateway"
+}
+
+resource "aws_vpc_endpoint" "vpce_gtw" {
+  for_each = { for tuple in local.settings.eks_cluster.gateway_vpce : tuple.id => tuple }
+  vpc_id   = data.terraform_remote_state.vpc.outputs.network_vpc_id
+  service_name = format(
+    "com.amazonaws.%s.%s",
+    local.regions[local.settings.region],
+    each.key
+  )
+  policy            = contains(keys(each.value), "policy") ? file(lookup(each.value, "policy")) : null
+  vpc_endpoint_type = "Gateway"
+
+  tags = {
+    Name = format(
+      "vpce-%s-%s-%s-01",
+      local.settings.env,
+      local.settings.region,
+      replace(
+        each.key,
+        strcontains(each.key, ".") ? "." : null,
+        "-"
+      )
+    )
+  }
+}
+
+locals {
+  # iterate over vpc endpoint gateways nested to the list of route table id
+  route_table_ids = flatten([
+    for tuple in local.settings.eks_cluster.gateway_vpce : [
+      for rt in data.terraform_remote_state.vpc.outputs.network_application_route_table_ids : {
+        vpc_endpoint_id = tuple.id
+        route_table_id  = rt
+      }
+    ]
+  ])
+}
+
+resource "aws_vpc_endpoint_route_table_association" "gateway_rt_assoc" {
+  for_each = { for k in nonsensitive(local.route_table_ids) : format("%s-%s", k.route_table_id, k.vpc_endpoint_id) =>
+    {
+      vpce_id = k.vpc_endpoint_id
+      rt_id   = k.route_table_id
+    }
+  }
+
+  vpc_endpoint_id = aws_vpc_endpoint.vpce_gtw[each.value["vpce_id"]].id
+  route_table_id  = each.value["rt_id"]
 }
