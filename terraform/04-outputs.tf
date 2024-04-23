e33d37e8571ec16b927a21141850f258edec7f09
saranshan313 <sar.mca05@gmail.com>
2024-04-23 19:18:33 +1000

    add outputs for eks cluster

--- terraform/04-outputs.tf
+++ terraform/04-outputs.tf
@@ -1,4 +1,24 @@
-# output "ecs_lb_dns_name" {
-#   description = "DNS Name of the ECS Load Balancer"
-#   value       = try(aws_lb.ecs_alb.dns_name, null)
-# }
+output "eks_lb_dns_name" {
+  description = "DNS Name of the EKS Load Balancer"
+  value       = try(aws_lb.eks_alb.dns_name, null)
+}
+
+output "eks_endpoint" {
+  description = "Endpoint of the EKS Cluster"
+  value       = try(aws_eks_cluster.eks_apps.endpoint, null)
+}
+
+output "eks_ca" {
+  description = "CA of the EKS Cluster"
+  value       = try(aws_eks_cluster.eks_apps.certificate_authority.data, null)
+}
+
+output "eks_oidc" {
+  description = "OIDC of the EKS Cluster"
+  value       = try(aws_eks_cluster.eks_apps.oidc.issuer, null)
+}
+
+output "eks_cluster_name" {
+  description = "Name of the EKS Cluster"
+  value       = try(aws_eks_cluster.eks_apps.id, null)
+}
