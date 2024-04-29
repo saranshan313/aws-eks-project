879761378653066f82dd9725522f0a4713293015
saranshan313 <sar.mca05@gmail.com>
2024-04-29 21:52:53 +1000

    deploy apse2 resources

--- terraform/01-main.tf
+++ terraform/01-main.tf
@@ -1,6 +1,7 @@
 locals {
   regions = {
-    "use1" = "us-east-1"
+    "use1"  = "us-east-1"
+    "apse2" = "ap-southeast-2"
   }
   settings = yamldecode(file("${var.TFC_WORKSPACE_NAME}.yaml"))
 }
@@ -30,7 +31,7 @@ provider "aws" {
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket = "tf-remote-state-234-343-555-666-777"
+    bucket = "tf-remote-state-234-343-555"
     key    = "env:/infra-${local.settings.env}-${local.settings.region}/infra-${local.settings.env}-${local.settings.region}.tfstate"
     region = local.regions[local.settings.region]
   }
