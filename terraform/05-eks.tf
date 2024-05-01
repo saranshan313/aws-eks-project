b45540ca273a18c030ce50896579928b75e73578
saranshan313 <sar.mca05@gmail.com>
2024-05-01 20:53:24 +1000

    update the subnet for eks

--- terraform/05-eks.tf
+++ terraform/05-eks.tf
@@ -6,7 +6,8 @@ resource "aws_eks_cluster" "eks_apps" {
 
   vpc_config {
     subnet_ids = [
-      for k, v in data.terraform_remote_state.vpc.outputs.network_application_subnets : v
+      #for k, v in data.terraform_remote_state.vpc.outputs.network_application_subnets : v
+      for k, v in data.terraform_remote_state.vpc.outputs.network_public_subnets : v
     ]
     endpoint_private_access = local.settings.eks_cluster.vpc_config.private_access
     endpoint_public_access  = local.settings.eks_cluster.vpc_config.public_access
@@ -33,16 +34,16 @@ resource "aws_eks_cluster" "eks_apps" {
 }
 
 #EKS Add ons
-# resource "aws_eks_addon" "eks_apps" {
-#   for_each                    = local.settings.eks_cluster.add_on
-#   cluster_name                = aws_eks_cluster.eks_apps.name
-#   addon_name                  = each.value["name"]
-#   resolve_conflicts_on_create = each.value["resolve_conflict"]
-#   addon_version               = each.value["version"]
-#   depends_on = [
-#     aws_eks_node_group.eks_apps
-#   ]
-# }
+resource "aws_eks_addon" "eks_apps" {
+  for_each                    = local.settings.eks_cluster.add_on
+  cluster_name                = aws_eks_cluster.eks_apps.name
+  addon_name                  = each.value["name"]
+  resolve_conflicts_on_create = each.value["resolve_conflict"]
+  addon_version               = each.value["version"]
+  depends_on = [
+    aws_eks_node_group.eks_apps
+  ]
+}
 
 #EKS Node Groups
 resource "aws_eks_node_group" "eks_apps" {
@@ -51,7 +52,8 @@ resource "aws_eks_node_group" "eks_apps" {
   node_group_name = "nodegrp-${local.settings.env}-${local.settings.eks_cluster.node_groups[each.key].name}-01"
   node_role_arn   = aws_iam_role.node_group_role.arn
   subnet_ids = [
-    for k, v in data.terraform_remote_state.vpc.outputs.network_application_subnets : v
+    #for k, v in data.terraform_remote_state.vpc.outputs.network_application_subnets : v
+    for k, v in data.terraform_remote_state.vpc.outputs.network_public_subnets : v
   ]
 
   scaling_config {
