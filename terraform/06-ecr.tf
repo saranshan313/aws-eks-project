7dbb54bf2ca958c92ec34bef56382b76ce3f5cbe
saranshan313 <sar.mca05@gmail.com>
2024-05-23 20:10:48 +1000

    update the ecr repo

--- terraform/06-ecr.tf
+++ terraform/06-ecr.tf
@@ -1,12 +1,12 @@
 #Image repository for ECS application
 resource "aws_ecr_repository" "eks_app" {
-  name = "repo-${local.settings.env}-${local.settings.region}-app-01"
+  name = "repo-${local.settings.env}-${local.settings.region}-eksapp-01"
 
   image_scanning_configuration {
     scan_on_push = local.settings.ecr_eks.scan_on_push
   }
 
   tags = {
-    Name = "repo-${local.settings.env}-${local.settings.region}-app-01"
+    Name = "repo-${local.settings.env}-${local.settings.region}-eksapp-01"
   }
 }
